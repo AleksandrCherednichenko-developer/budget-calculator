05405f432a2de9aa1b3949770dcfd924a1e05608
AlexanderCherednicheenko <acherednichenko174@gmail.com>
2021-08-12 23:40:36 +0300

    приведение script.js к общему виду

--- js/script.js
+++ js/script.js
@@ -1,35 +1,25 @@
 'use strict'
 
-let btnStart = document.getElementById('start');
+let start = document.getElementById('start'),
+   btnPluse = document.getElementsByClassName('btn_plus'),
+   incomePlsue = btnPluse[0],
+   expensesPluse = btnPluse[1],
+   additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
+   depositCheck = document.querySelector('#deposit-check'),
+   budgetMonthValue = document.getElementsByClassName('budget_month-value')[0],
+   budgetDayValue = document.getElementsByClassName('budget_day-value')[0],
+   expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0],
+   additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0],
+   additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0],
+   incomePeriodValue = document.getElementsByClassName('income_period-value')[0],
+   targetMonthValue = document.getElementsByClassName('target_month-value')[0],
+   salaryAmount = document.querySelector('.salary-amount'),
+   incomeTilte = document.querySelector('.income-title'),
+   incomeAmount = document.querySelector('.income-amount'),
+   expensesTitle = document.querySelector('.expenses-title'),
+   expensesAmount = document.querySelector('.expenses-amount'),
+   additionalExpenses = document.querySelector('.additional_expenses-item'),
+   targetAmount = document.querySelector('.target-amount'),
+   periodSelect = document.querySelector('.period-select');
 
-let btnSum = document.getElementsByClassName('btn_plus')[0];
-let btnSum1 = document.getElementsByClassName('btn_plus')[1];
 
-let depositCheck = document.querySelector('#deposit-check');
-
-let additionalItem = document.querySelectorAll('.additional_income-item');
-
-// let e = document.querySelectorAll('[class*="-value"]');
-let valBudgetMonth = document.getElementsByClassName('budget_month-value');
-let valBudgetBay = document.getElementsByClassName('budget_day-value');
-let valExpensesMonth = document.getElementsByClassName('expenses_month-value');
-let valAdditionalIncome = document.getElementsByClassName('additional_income-value');
-let valAdditionalExpenses = document.getElementsByClassName('additional_expenses-value');
-let valIncomePeriod = document.getElementsByClassName('income_period-value');
-let valTargetMonth = document.getElementsByClassName('target_month-value');
-
-let inputSalary = document.querySelector('.salary input');
-
-let nameIncomeItems = document.querySelector('.income-items input');
-
-let sumIncomeItems = document.querySelector('.income-amount');
-
-let nameExpensesItem = document.querySelector('.expenses-items input');
-
-let sumExpensesItem = document.querySelector('.expenses-amount');
-
-let inputAdditionalExpenses = document.querySelector('.additional_expenses-item');
-
-let inputTargetAmount = document.querySelector('.target-amount');
-
-let inputPeriodSelect = document.querySelector('.period-select');
